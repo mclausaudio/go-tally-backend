658cb7dc96f37e0125d93c0ec765db341b077a9b
Michael_Claus <mclausaudio@gmail.com>
2022-06-04 23:53:34 -0700

    Define primaryTable for dynamoDB

--- lib/dynamodb-stack.ts
+++ lib/dynamodb-stack.ts
@@ -1,16 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 
 export class DynamoDBStack extends Stack {
+  public readonly primaryTable: ddb.Table;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Primary DDB Table
+    const primaryTable = new ddb.Table(this, 'primary-table', {
+      tableName: `go-tally-beta-primary-table`,
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'PK', type: ddb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: ddb.AttributeType.STRING }
+    })
+    this.primaryTable = primaryTable;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
