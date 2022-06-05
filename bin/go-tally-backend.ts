573e93cf9fa8ab9d2c5079b2e74daa4819fde0c3
Michael_Claus <mclausaudio@gmail.com>
2022-06-04 23:52:39 -0700

    Add LambdaStack, update DynamoDBStack name

--- bin/go-tally-backend.ts
+++ bin/go-tally-backend.ts
@@ -2,7 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DynamoDBStack } from '../lib/dynamodb-stack';
+import { LambdaStack } from '../lib/lambda-stack';
 
 const app = new cdk.App();
 
-new DynamoDBStack(app, 'GoTallyBackendStack', {});
+new DynamoDBStack(app, 'GoTallyDynamoDBStack', {});
+new LambdaStack(app, 'GoTallyLambdaStack', {});
