cc18fcaa033efb62e5c0fad81a8bc4ae26cb9d07
Michael_Claus <mclausaudio@gmail.com>
2022-06-05 20:28:51 -0700

    Extend dynamoLambda decclaration to accept TABLE_NAME enviornment var, great readWriteAcess

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,10 +1,15 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
 
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
+export interface LambdaStackProps extends StackProps {
+  ddbTable: Table;
+}
+
 export class LambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: LambdaStackProps) {
     super(scope, id, props);
 
     // Declare a lambda
@@ -12,7 +17,12 @@ export class LambdaStack extends Stack {
       runtime: lambda.Runtime.NODEJS_14_X,      // execution environment
       code: lambda.Code.fromAsset('lambda-source-code'),  // code loaded from the "lambda-source-code" directory
       handler: 'hello.handler',                // file is "hello", function is "handler"
+      environment: {
+        TABLE_NAME: props?.ddbTable.tableName ? props.ddbTable.tableName : '',
+      },
     });
+
+    props?.ddbTable.grantReadWriteData(dynamoLambda);
   }
 
 }
