092e17538f9374af303d6ef57ad80b56098e44eb
Michael_Claus <mclausaudio@gmail.com>
2022-06-06 17:57:46 -0700

    Update lambdaStack to connect to API Gateway endpoint, simple implementation.  Deleted hello.ts

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -3,9 +3,11 @@ import { Construct } from 'constructs';
 import { Table } from 'aws-cdk-lib/aws-dynamodb';
 
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export interface LambdaStackProps extends StackProps {
-  ddbTable: Table;
+  ddbTable: Table,
+  restApi: apigw.RestApi
 }
 
 export class LambdaStack extends Stack {
@@ -13,16 +15,27 @@ export class LambdaStack extends Stack {
     super(scope, id, props);
 
     // Declare a lambda
-    const dynamoLambda = new lambda.Function(this, 'dynamo-lambda-handler', {
+    const dynamoLambda = new lambda.Function(this, 'get-user-metadata-lambda-handler', {
       runtime: lambda.Runtime.NODEJS_14_X,      // execution environment
       code: lambda.Code.fromAsset('lambda-source-code'),  // code loaded from the "lambda-source-code" directory
-      handler: 'hello.handler',                // file is "hello", function is "handler"
+      handler: 'get-user-metadata.handler',                // file is "hello", function is "handler"
       environment: {
         TABLE_NAME: props?.ddbTable.tableName ? props.ddbTable.tableName : '',
       },
     });
 
     props?.ddbTable.grantReadWriteData(dynamoLambda);
+
+    const restApi = props?.restApi;
+    restApi?.root.addMethod("ANY");
+    const users = restApi?.root.addResource('users');
+    const user = users?.addResource('{userId}')
+    // integrate GET /todos with getTodosLambda
+    user?.addMethod(
+      'GET',
+      new apigw.LambdaIntegration(dynamoLambda, { proxy: true }),
+    );
+    
   }
 
 }
