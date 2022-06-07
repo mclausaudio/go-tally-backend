b0bcbb3034e12fa486b76942c5934afcfe9f7517
Michael_Claus <mclausaudio@gmail.com>
2022-06-06 17:56:01 -0700

    Add apiStack, pass restApi to lambdaStack

--- bin/go-tally-backend.ts
+++ bin/go-tally-backend.ts
@@ -3,10 +3,13 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DynamoDBStack } from '../lib/dynamodb-stack';
 import { LambdaStack } from '../lib/lambda-stack';
+import { APIStack } from '../lib/api-stack';
 
 const app = new cdk.App();
 
 const dynamoDBStack = new DynamoDBStack(app, 'GoTallyDynamoDBStack', {});
+const apiStack = new APIStack(app, 'GoTallyBackEndAPI', {});
 const lambdaStack = new LambdaStack(app, 'GoTallyLambdaStack', { 
-  ddbTable: dynamoDBStack.primaryTable
+  ddbTable: dynamoDBStack.primaryTable,
+  restApi: apiStack.restApi
 });
