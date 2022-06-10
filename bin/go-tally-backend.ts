7f95035ff9dcc362600a2c2840018c37fc07d356
Michael_Claus <mclausaudio@gmail.com>
2022-06-09 22:10:32 -0700

    Pass the API Stack the cognito userPool and userPoolClient

--- bin/go-tally-backend.ts
+++ bin/go-tally-backend.ts
@@ -11,8 +11,8 @@ const app = new cdk.App();
 const dynamoDBStack = new DynamoDBStack(app, 'GoTallyDynamoDBStack', {});
 const cognitoStack = new CognitoStack(app, 'GoTallyCognitoStack', {});
 const apiStack = new APIStack(app, 'GoTallyBackEndAPI', {
-  // cognitoUserPool: cognitoStack.cognitoUserPool,
-  // cognitoUserPoolClient: cognitoStack.cognitoUserPoolClient
+  cognitoUserPool: cognitoStack.cognitoUserPool,
+  cognitoUserPoolClient: cognitoStack.cognitoUserPoolClient
 });
 const lambdaStack = new LambdaStack(app, 'GoTallyLambdaStack', { 
   ddbTable: dynamoDBStack.primaryTable,
