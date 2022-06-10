9bf3a2ef4cbff15eac4f02434e9022a57b9b51ba
Michael_Claus <mclausaudio@gmail.com>
2022-06-09 22:12:18 -0700

    Add authorizer to Gateway to limit lambda requests to logged in users in cognito userPool

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -4,17 +4,17 @@ import * as cdk from 'aws-cdk-lib';
 import { RestApi, AuthorizationType, CognitoUserPoolsAuthorizer } from 'aws-cdk-lib/aws-apigateway';
 import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 export interface APIStackProps extends StackProps {
-  // cognitoUserPool: UserPool,
-  // cognitoUserPoolClient: UserPoolClient,
+  cognitoUserPool: UserPool,
+  cognitoUserPoolClient: UserPoolClient,
 }
 export class APIStack extends Stack {
   public readonly restApi: RestApi;
   constructor(scope: Construct, id: string, props: APIStackProps) {
     super(scope, id, props);
 
-    // const authorizer = new CognitoUserPoolsAuthorizer(this, 'crud-authorizer', {
-    //   cognitoUserPools: [props.cognitoUserPool]
-    // });
+    const authorizer = new CognitoUserPoolsAuthorizer(this, 'crud-authorizer', {
+      cognitoUserPools: [props.cognitoUserPool]
+    });
 
     // Define HTTP API
     const restApi = new RestApi(this, 'backend-api', {
@@ -33,10 +33,10 @@ export class APIStack extends Stack {
         allowCredentials: true,
         allowOrigins: ['*'],
       },
-      // defaultMethodOptions: {
-      //   authorizationType: AuthorizationType.COGNITO,
-      //   authorizer,
-      // }
+      defaultMethodOptions: {
+        authorizationType: AuthorizationType.COGNITO,
+        authorizer,
+      }
     })
     this.restApi = restApi;
 
